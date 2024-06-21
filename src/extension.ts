8dc6ec584b7dfb8a347b134ff77c4400fde5b580
Daniel Payet <danielpayet.dev@gmail.com>
2024-06-22 00:01:12 +0200

    switch to webview

--- src/extension.ts
+++ src/extension.ts
@@ -5,20 +5,22 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "ng-lint-ast-viewer.show-ast",
     async () => {
-      const p = parseForESLint(
+      const angularEslint = parseForESLint(
         vscode.window.activeTextEditor?.document.getText() ?? "",
         {
           suppressParseErrors: true,
           filePath: vscode.window.activeTextEditor?.document.uri.fsPath!,
-        },
+        }
       );
 
-      const a = await vscode.workspace.openTextDocument({
-        language: "json",
-        content: JSON.stringify(p.ast, undefined, 2),
-      });
-      vscode.window.showTextDocument(a, vscode.ViewColumn.Beside);
-    },
+      const webView = vscode.window.createWebviewPanel(
+        "ngLintAst",
+        "AST " + vscode.window.activeTextEditor?.document.fileName,
+        vscode.ViewColumn.Beside
+      );
+
+	  webView.webview.html = JSON.stringify(angularEslint.ast, undefined, 2);      
+    }
   );
 
   context.subscriptions.push(disposable);

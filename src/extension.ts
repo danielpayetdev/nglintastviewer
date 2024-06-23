d3e45455a689598ded7dd427a140ff5930518d7d
Daniel Payet <danielpayet.dev@gmail.com>
2024-06-23 14:33:21 +0200

    Ast tree view (#1)
    * print full tree
    
    * collapse tree
    
    * clean style

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from "vscode";
-import { parseForESLint } from "@angular-eslint/template-parser";
+import { parse } from "@angular-eslint/template-parser";
+import path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "ng-lint-ast-viewer.show-ast",
     async () => {
-      const angularEslint = parseForESLint(
+      const ast = parse(
         vscode.window.activeTextEditor?.document.getText() ?? "",
         {
           suppressParseErrors: true,
@@ -15,11 +16,13 @@ export function activate(context: vscode.ExtensionContext) {
 
       const webView = vscode.window.createWebviewPanel(
         "ngLintAst",
-        "AST " + vscode.window.activeTextEditor?.document.fileName,
+        "AST " +
+          path.parse(vscode.window.activeTextEditor?.document.fileName ?? "")
+            .base,
         vscode.ViewColumn.Beside
       );
 
-	  webView.webview.html = JSON.stringify(angularEslint.ast, undefined, 2);      
+      webView.webview.html = buildHtml(ast);
     }
   );
 
@@ -27,3 +30,96 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
+
+const buildHtml = (ast: Record<string, any>): string => {
+  return `
+		<html>
+		<head>
+		${getGlobalStyle()}
+		</head>
+		<body>${buildTree(ast)}</body>
+		</html>
+		`;
+};
+
+const getGlobalStyle = (): string => {
+  return `
+		<style>
+		body {
+			font-size: '14px';
+			font-family: monospace;
+		}
+		li {
+			cursor: pointer;
+			list-style-type: none;
+		}
+		</style>
+	`;
+};
+
+const keyOrder: Record<string, number> = {
+  type: 0,
+  branches: 1,
+  children: 1,
+  expression: 2,
+  expressions: 2,
+  expressionAlias: 3,
+  i18n: 4,
+};
+
+const buildTree = (ast: Record<string, any>): string => {
+  return (
+    "<ul>" +
+    Object.keys(ast)
+      .sort((key1, key2) => {
+        const key1Order = keyOrder[key1] ?? 100;
+        const key2Order = keyOrder[key2] ?? 100;
+        return key1Order - key2Order;
+      })
+      .map((key) => {
+        return `
+		<li style='padding-top:.5rem;padding-bottom:.5rem'>
+			${
+        ast[key] &&
+        typeof ast[key] === "object" &&
+        (Array.isArray(ast[key]) ? ast[key].length : true)
+          ? `
+			<details>
+				<summary>${key}: ${getNodeValue(ast[key])}</summary>
+				${buildSubTree(ast[key])}
+			</details>`
+          : `${key}: ${getNodeValue(ast[key])}`
+      }
+			
+		</li>`;
+      })
+      .join("") +
+    "</ul>"
+  );
+};
+
+const buildSubTree = (node: any) => {
+  return `
+	<div style="margin-left:.5rem">
+		${buildTree(node)}
+	</div>
+	`;
+};
+
+const getNodeValue = (node: any): string => {
+  if (node && typeof node === "object") {
+    if (Array.isArray(node)) {
+      return gerArrayValue(node);
+    }
+    return "Object";
+  }
+  return node;
+};
+
+const gerArrayValue = (arrayNode: any[]): string => {
+  let value = "";
+  if (arrayNode.length > 0) {
+    value = `${arrayNode.length} element${arrayNode.length > 1 ? "" : "s"}`;
+  }
+  return `[${value}]`;
+};

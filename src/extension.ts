8c354bf6d1dfc03daf8aa408644ab50b8577432b
Daniel Payet <danielpayet.dev@gmail.com>
2024-06-23 13:23:09 +0200

    collapse tree

--- src/extension.ts
+++ src/extension.ts
@@ -51,20 +51,12 @@ const getGlobalStyle = (): string => {
 		}
 		li {
 			cursor: pointer;
+			list-style-type: none;
 		}
 		</style>
 	`;
 };
 
-const getScript = () => {
-  return `
-	deploy(index) {
-
-	}
-	`;
-};
-
-let globalTreeID = 0;
 const keyOrder: Record<string, number> = {
   type: 0,
   branches: 1,
@@ -76,19 +68,28 @@ const keyOrder: Record<string, number> = {
 
 const buildTree = (ast: Record<string, any>): string => {
   return (
-    "<ul>" +
+    "<ul style='padding:.5rem'>" +
     Object.keys(ast)
       .sort((key1, key2) => {
         const key1Order = keyOrder[key1] ?? 100;
         const key2Order = keyOrder[key2] ?? 100;
         return key1Order - key2Order;
       })
-      .map((k, index) => {
-        const treeID = `${globalTreeID++}_${index}`;
+      .map((key) => {
         return `
 		<li>
-			<a id="${treeID}" onclick="show(treeID)">${k}: ${getNodeValue(ast[k])}</a>
-			${ast[k] && typeof ast[k] === "object" ? buildSubTree(ast[k]) : ""}
+			${
+        ast[key] &&
+        typeof ast[key] === "object" &&
+        (Array.isArray(ast[key]) ? ast[key].length : true)
+          ? `
+			<details>
+				<summary>${key}: ${getNodeValue(ast[key])}</summary>
+				${buildSubTree(ast[key])}
+			</details>`
+          : `${key}: ${getNodeValue(ast[key])}`
+      }
+			
 		</li>`;
       })
       .join("") +
@@ -98,7 +99,7 @@ const buildTree = (ast: Record<string, any>): string => {
 
 const buildSubTree = (node: any) => {
   return `
-	<div style="display:block;margin-left:1rem">
+	<div style="margin-left:.5rem">
 		${buildTree(node)}
 	</div>
 	`;
